df81034d2db685347845d300f3503924c6a712bd
lizaiganshenmo <785006527@qq.com>
2023-08-21 20:06:40 +0800

    2023-08-21

---
+++ leetcode/189.go
@@ -0,0 +1,36 @@
+package leetcode
+
+// https://leetcode.cn/problems/rotate-array/?envType=study-plan-v2&envId=top-interview-150
+
+//189. 轮转数组
+// 给定一个整数数组 nums，将数组中的元素向右轮转 k 个位置，其中 k 是非负数。
+
+// 示例 1:
+
+// 输入: nums = [1,2,3,4,5,6,7], k = 3
+// 输出: [5,6,7,1,2,3,4]
+// 解释:
+// 向右轮转 1 步: [7,1,2,3,4,5,6]
+// 向右轮转 2 步: [6,7,1,2,3,4,5]
+// 向右轮转 3 步: [5,6,7,1,2,3,4]
+// 示例 2:
+
+// 输入：nums = [-1,-100,3,99], k = 2
+// 输出：[3,99,-1,-100]
+// 解释:
+// 向右轮转 1 步: [99,-1,-100,3]
+// 向右轮转 2 步: [3,99,-1,-100]
+
+// 提示：
+
+// 1 <= nums.length <= 105
+// -231 <= nums[i] <= 231 - 1
+// 0 <= k <= 105
+
+func rotate(nums []int, k int) {
+	newNums := make([]int, len(nums))
+	for i, v := range nums {
+		newNums[(i+k)%len(nums)] = v
+	}
+	copy(nums, newNums)
+}
